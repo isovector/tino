8935fe05f6ff9edaa1c57e032551dbf9199308fe
Sandy Maguire <sandy@sandymaguire.me>
2015-08-09 14:03:08 +0300

    xmonad install should probably work

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
 GHC_VERSION=7.8.4
 
-PATH="$PATH:$HOME/.local/bin:$HOME/.stack/programs/x86_64-linux/ghc-$GHC_VERSION/bin"
+PATH="$PATH:$HOME/.local/bin:$HOME/.cabal/bin:$HOME/.stack/programs/x86_64-linux/ghc-$GHC_VERSION/bin"
