1a318feec09ba2ce76407a7b84796ac5a918f50e
Sandy Maguire <sandy@sandymaguire.me>
2015-08-20 10:03:58 -0400

    added vpn and some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 alias ls='ls --color=tty'
 alias pacman='sudo pacman'
 
+alias calc='python'
+
+alias ec2-serenade="ssh -i ~/.ssh/santino.pem ubuntu@$SERENADE"
+alias ec2-haystack='ssh -i ~/.ssh/Haystack.pem ubuntu@52.10.229.25'
+
