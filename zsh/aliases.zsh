ac3d4ef28dfbced5c6dcff7e535e358d670d56df
Sandy Maguire <sandy@sandymaguire.me>
2015-10-12 21:56:54 -0700

    aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,5 +8,14 @@ alias calc='python'
 alias ec2-serenade="ssh -i ~/.ssh/santino.pem ubuntu@$SERENADE"
 alias ec2-haystack='ssh -i ~/.ssh/Haystack.pem ubuntu@52.10.229.25'
 
+alias xclip='xcopy -selection c'
+
 alias t='tino'
 alias o='gnome-open'
+alias v='vim'
+alias e='vim'
+alias :e='vim'
+
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
